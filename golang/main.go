a958b551c64b41d4ecfa7be2bd78942ff6d014bf
MaksimLahtin <maksim.lahtin2006@gmail.com>
2024-12-20 19:39:27 +0000

    fix

--- golang/main.go
+++ golang/main.go
@@ -6,6 +6,5 @@ import (
 	"isuct.ru/informatics2022/lab4"
 )
 func main() {
-	fmt.Println("Задача А:\n", lab4.TaskA(0.26, 0.66, 0.08))
-	fmt.Println("Задача В:\n", lab4.TaskB([]float64{0.1, 0.35, 0.4, 0.55, 0.6}))
+	    lab4.RunLab4()
 }

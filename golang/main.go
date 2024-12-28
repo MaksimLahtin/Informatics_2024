eaff56441d8065c92fdd7dd069000b7af3b4a9d2
MaksimLahtin <maksim.lahtin2006@gmail.com>
2024-12-28 11:33:32 +0000

    fix

--- golang/main.go
+++ golang/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"isuct.ru/informatics2022/lab6"
 	"isuct.ru/informatics2022/lab4"
 )
 func main() {
+	fmt.Println("Лахтин Максим Сергеевич")
 	lab4.RunLab4Tasks()
 	lab6.RunLab6()
 }

db0e3558fca8e91f8acee26a40ce669a33b68e81
MaksimLahtin <maksim.lahtin2006@gmail.com>
2025-01-12 16:52:43 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -10,14 +10,4 @@ func main() {
         fmt.Println("Лаборатнорная 7:")
 		lab7.Lab7()
 }
-	
-	
-	"isuct.ru/informatics2022/lab6"
-	"isuct.ru/informatics2022/lab4"
-)
-func main() {
-	fmt.Println("Лахтин Максим Сергеевич")
-	lab4.RunLab4()
-	lab6.RunLab6()
-}
 
